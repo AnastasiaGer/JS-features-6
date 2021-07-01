34b576923e91ec978a834185ae10f08c71e1b393
AnastasiaGer <aagermanova@gmail.com>
2021-07-01 14:43:39 -0700

    4

--- Whack A Mole/scrips.js
+++ Whack A Mole/scrips.js
@@ -18,3 +18,12 @@ const holes = document.querySelectorAll('.hole');
     lastHole = hole;
     return hole;
   }
+  function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+      hole.classList.remove('up');
+      if (!timeUp) peep();
+    }, time);
+  }
